8e02d8108f8a38f8320b1a383d7134a68d319428
Marat Zhumataev <marat.air.92@mail.ru>
2023-04-09 14:35:07 +0300

    Добавили задачу 2

---
+++ Task 02/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число для сравнения");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число для сравнения");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"Большее число = {number1}");
+    Console.WriteLine($"Меньшее число = {number2}");
+}
+else if (number2 > number1)
+{
+    Console.WriteLine($"Большее число = {number2}");
+    Console.WriteLine($"Меньшее число = {number1}");
+    }
+else
+{
+    Console.WriteLine("Числа равны");
+}
