11b38ad2daa5202a891e03d9d98f8ab984b1bc79
Marat Zhumataev <marat.air.92@mail.ru>
2023-04-09 15:11:38 +0300

    Добавили задачу 8

---
+++ Task 08/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+int index = 2;
+while (index <= number)
+{
+    Console.Write ($"{index} ");
+    index = index + 2;
+}
